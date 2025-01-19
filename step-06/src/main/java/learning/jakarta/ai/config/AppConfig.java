f4c5501d6799026f105be5b9780077cee7ef95e8
Bazlur Rahman <bazlur@jugbd.org>
2025-01-19 02:27:10 -0500

    Add new steps for splitting documents demo

---
+++ step-06/src/main/java/learning/jakarta/ai/config/AppConfig.java
@@ -0,0 +1,52 @@
+package learning.jakarta.ai.config;
+
+import dev.langchain4j.model.embedding.EmbeddingModel;
+import dev.langchain4j.model.embedding.onnx.allminilml6v2.AllMiniLmL6V2EmbeddingModel;
+import dev.langchain4j.store.embedding.pgvector.PgVectorEmbeddingStore;
+import jakarta.annotation.Resource;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.inject.Default;
+import jakarta.enterprise.inject.Produces;
+import lombok.extern.slf4j.Slf4j;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import javax.sql.DataSource;
+
+@Slf4j
+@ApplicationScoped
+public class AppConfig {
+    @Resource(lookup = "java:jboss/datasources/LLMJakartaPostgresDS")
+    private DataSource dataSource;
+
+    @Produces
+    @Default
+    public DataSource produceDataSource() {
+        return dataSource;
+    }
+
+    @Produces
+    @ApplicationScoped
+    public EmbeddingModel embeddingModel() {
+        return new AllMiniLmL6V2EmbeddingModel();
+    }
+
+    @Produces
+    public PgVectorEmbeddingStore produceEmbeddingStore(EmbeddingModel embeddingModel,
+                                                        DataSource dataSource,
+                                                        @ConfigProperty(name = "pgvector.dropTableFirst") boolean dropTableFirst,
+                                                        @ConfigProperty(name = "pgvector.createTable") boolean createTable
+    ) {
+        log.info("Creating PgVectorEmbeddingStore using DataSource: {}", dataSource);
+
+        return PgVectorEmbeddingStore.datasourceBuilder()
+                .datasource(dataSource)
+                .dropTableFirst(dropTableFirst)
+                .createTable(createTable)
+                .useIndex(true)
+                .dimension(embeddingModel.dimension())
+                .indexListSize(100)
+                .table("embedding_store")
+                .build();
+    }
+}
+
---
+++ step-06/src/main/java/learning/jakarta/ai/config/StartupBean.java
@@ -0,0 +1,63 @@
+package learning.jakarta.ai.config;
+
+import dev.langchain4j.data.document.Document;
+import dev.langchain4j.data.document.DocumentSplitter;
+import dev.langchain4j.data.document.loader.FileSystemDocumentLoader;
+import dev.langchain4j.data.document.parser.apache.tika.ApacheTikaDocumentParser;
+import dev.langchain4j.data.document.splitter.DocumentByParagraphSplitter;
+import dev.langchain4j.data.document.splitter.DocumentSplitters;
+import dev.langchain4j.data.embedding.Embedding;
+import dev.langchain4j.data.segment.TextSegment;
+import dev.langchain4j.model.embedding.EmbeddingModel;
+import dev.langchain4j.model.openai.OpenAiTokenizer;
+import dev.langchain4j.store.embedding.EmbeddingStoreIngestor;
+import dev.langchain4j.store.embedding.pgvector.PgVectorEmbeddingStore;
+import jakarta.annotation.PostConstruct;
+import jakarta.ejb.Singleton;
+import jakarta.ejb.Startup;
+import jakarta.inject.Inject;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import static dev.langchain4j.model.openai.OpenAiChatModelName.GPT_4_O_MINI;
+
+@Slf4j
+@Startup
+@Singleton
+public class StartupBean {
+
+    @Inject
+    private ConfigProp config;
+    @Inject
+    private EmbeddingModel embeddingModel;
+    @Inject
+    private PgVectorEmbeddingStore pgVectorStore;
+    @Inject
+    private DataSource dataSource;
+
+    @PostConstruct
+    public void init() {
+        log.info("Application started successfully.");
+
+        List<Document> documents = FileSystemDocumentLoader.loadDocuments(config.getDocumentsDir(), new ApacheTikaDocumentParser());
+        log.info("Total documents parsed: {}", documents.size());
+
+        DocumentByParagraphSplitter paragraphSplitter = new DocumentByParagraphSplitter(config.getMaxSegmentSizeInTokens(), config.getMaxOverlapSizeInTokens());
+
+        EmbeddingStoreIngestor embeddingStoreIngestor = EmbeddingStoreIngestor.builder()
+                .documentSplitter(paragraphSplitter::split)
+                .textSegmentTransformer(textSegment -> TextSegment.from(
+                        textSegment.metadata().getString("file_name") + "\n" + textSegment.text(),
+                        textSegment.metadata()))
+                .embeddingModel(embeddingModel)
+                .embeddingStore(pgVectorStore)
+                .build();
+
+        embeddingStoreIngestor.ingest(documents);
+    }
+}
