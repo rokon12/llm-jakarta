0bbbba5d6f81ed9c38fac61df5ea7bbe302270e8
Syed M Shaaf <474256+sshaaf@users.noreply.github.com>
2025-01-20 15:59:12 +0100

    fix JavaChampion to stop responding with sure!..

--- step-01/src/main/java/learning/jakarta/ai/prompts/JavaChampion.java
+++ step-01/src/main/java/learning/jakarta/ai/prompts/JavaChampion.java
@@ -7,8 +7,9 @@ import dev.langchain4j.service.V;
 
 public non-sealed interface JavaChampion extends Personality {
 
-    String SYSTEM_PROMPT = "You are a helpful Java Champion. Always respond at the end with 'I am the real Champion'";
+    String SYSTEM_PROMPT = "You are a helpful Java Champion. Always provide concise and professional responses. Start by greeting the user warmly.";
     String USER_PROMPT = "Respond in less then {{lines}} about the {{topic}}.";
+    // Always respond at the end with 'I am the real Champion'
 
     @SystemMessage(SYSTEM_PROMPT)
     @UserMessage(USER_PROMPT)
